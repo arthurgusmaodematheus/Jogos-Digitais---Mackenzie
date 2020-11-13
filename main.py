73c91ef21ef5ed3a2ac87cae44126014d1971589
Gabriel <gabriel.rampazo@e-deploy.com.br>
2020-11-13 03:17:29 -0300

    [Add] Adicionando menu inicial e organizando o código

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-import random
+import sys
 import math
+import random
 import pygame
-from pygame import mixer
 
 pygame.init()
 
-# Screen
-screen = pygame.display.set_mode((800, 600))
+# --------------------------------------------------------
+#                       Variables
+# --------------------------------------------------------
 
-# Background
+# Screen
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+
+# Background Menu
+background_menu = pygame.image.load('images/background_menu.png')
+
+# Background Game
 background = pygame.image.load('images/background_game.png')
 
+# Menu
+start_button = pygame.image.load('images/button_start.png').convert_alpha()
+start_button_pos = (size[0]/4.0 - start_button.get_width()/2.0, size[1]*0.5 - start_button.get_height()/2.0)
+
+sound_button = [
+    pygame.image.load('images/sound_on.png').convert_alpha(),
+    pygame.image.load('images/sound_on.png').convert_alpha()
+    ]
+sound_button_pos = (715, 0)
+sound_button_index = 1
+
 # Sound
+sound = "sound/game_song.ogg"
 pygame.mixer.init(44000, -16, 1, 1024)
-mixer.music.load("song.ogg")
-mixer.music.play(-1)
+pygame.mixer.music.load(sound)
+pygame.mixer.music.play(-1)
 
 # Player
 playerImg = pygame.image.load('images/player.png')
@@ -22,6 +42,14 @@ playerX = 625
 playerY = 40
 playerX_change = 0
 playerY_change = 0
+
+# Enemies
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 5
 
 # Score
 score_value = 100
@@ -32,44 +60,94 @@ testY = 10
 font = pygame.font.Font('freesansbold.ttf', 32)
 
 
+# --------------------------------------------------------
+#                       Methods
+# --------------------------------------------------------
+
+def validate_click(mouse_pos, target_surface, target_pos):
+    return mouse_pos[0] >= target_pos[0] and mouse_pos[0] <= target_pos[0] + target_surface.get_rect()[2] and \
+           mouse_pos[1] >= target_pos[1] and mouse_pos[1] <= target_pos[1] + target_surface.get_rect()[3]
+
+
 def show_score(x, y):
     score = font.render("Score : " + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
 
 
-def player(x, y):
+def show_player(x, y):
     screen.blit(playerImg, (x, y))
 
 
-def enemy(x, y, i):
+def show_enemy(x, y, i):
     screen.blit(enemyImg[i], (x, y))
 
 
 def isCollision(enemyX, enemyY, playerX, playerY):
     distance = math.sqrt(math.pow(enemyX - playerX, 2) + (math.pow(enemyY - playerY, 2)))
-    if distance < 30:
+    if distance < 27:
         return True
     else:
         return False
 
 
-# Enemy
-enemyImg = []
-enemyX = []
-enemyY = []
-enemyX_change = []
-enemyY_change = []
-num_of_enemies = 8
-
-for i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load('images/ghost.png'))
-    enemyX.append(random.randint(0, 736))
-    enemyY.append(random.randint(50, 150))
-    enemyX_change.append(4)
-    enemyY_change.append(40)
+def create_enemies():
+    for enemy in range(num_of_enemies):
+        enemyImg.append(pygame.image.load('images/ghost.png'))
+        enemyX.append(random.randint(0, 736))
+        enemyY.append(random.randint(50, 150))
+        enemyX_change.append(4)
+        enemyY_change.append(40)
 
 
-# Game Loop
+# --------------------------------------------------------
+#                       Menu
+# --------------------------------------------------------
+show_menu = True
+local_sound_flag = True
+while show_menu:
+
+    # Background
+    screen.fill((0, 0, 0))
+    screen.blit(background_menu, (0, 0))
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # Click Position Mouse
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+
+            # Validate Click in Start Button
+            if validate_click(mouse_pos, start_button, start_button_pos):
+                show_menu = False
+
+            # Validate Click in Sound Button
+            if validate_click(mouse_pos, sound_button[sound_button_index], sound_button_pos):
+                sound_button_index = (sound_button_index + 1) % 2
+                local_sound_flag = not local_sound_flag
+                if not local_sound_flag:
+                    pygame.mixer.music.stop()
+                else:
+                    pygame.mixer.music.load(sound)
+                    pygame.mixer.music.play(-1, 0)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(start_button, start_button_pos)
+    screen.blit(sound_button[sound_button_index], sound_button_pos)
+
+    pygame.display.update()
+
+
+# --------------------------------------------------------
+#                       Game Loop
+# --------------------------------------------------------
+create_enemies()
 running = True
 while running:
 
@@ -80,6 +158,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # Click Position Mouse
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+
+            # Validate Click in Sound Button
+            if validate_click(mouse_pos, sound_button[sound_button_index], sound_button_pos):
+                sound_button_index = (sound_button_index + 1) % 2
+                local_sound_flag = not local_sound_flag
+                if not local_sound_flag:
+                    pygame.mixer.music.stop()
+                else:
+                    pygame.mixer.music.load(sound)
+                    pygame.mixer.music.play(-1, 0)
 
         # if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
@@ -111,15 +203,8 @@ while running:
         playerY = 736
 
     for i in range(num_of_enemies):
-    #
-    #     # Game Over
-    #     if enemyY[i] > 440:
-    #         for j in range(num_of_enemies):
-    #             enemyY[j] = 2000
-    #         game_over_text()
-    #         break
-    #
         enemyX[i] += enemyX_change[i]
+
         if enemyX[i] <= 0:
             enemyX_change[i] = 4
             enemyY[i] += enemyY_change[i]
@@ -127,12 +212,14 @@ while running:
             enemyX_change[i] = -4
             enemyY[i] += enemyY_change[i]
 
-        enemy(enemyX[i], enemyY[i], i)
+        show_enemy(enemyX[i], enemyY[i], i)
 
-    collision = isCollision(enemyX[i], enemyY[i], playerX, playerY)
-    if collision:
-        score_value -= 1
+        collision = isCollision(enemyX[i], enemyY[i], playerX, playerY)
+        if collision:
+            score_value -= 1
 
-    player(playerX, playerY)
+    screen.blit(sound_button[sound_button_index], sound_button_pos)
+    show_player(playerX, playerY)
     show_score(textX, testY)
+
     pygame.display.update()
